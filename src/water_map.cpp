3d16780c9619ad289ac2ea4241b93e7028ed8026
smeagolthellama <magardner2010@gmail.com>
2017-09-13 09:03:38 +0300

    kepp on adding/translating comments

--- src/water_map.cpp
+++ src/water_map.cpp
@@ -7,7 +7,7 @@ water_map::water_map(int size_x, int size_y)
 
 	for (i = 0; i < size_x; i++) {
 		for (j = 0; j < size_y; j++) {
-			m_hardness_map[i][j] = (.1 / (float)i) * ((float)j / 4.0) * ((float)j / 4.0);///!< szep, volgy alaku fuggveny. kep is lesz beteve.
+			m_hardness_map[i][j] = (.1 / (float)i) * ((float)j / 4.0) * ((float)j / 4.0);///!< szep, volgy alaku fuggveny. kep is van: images/volgy.svg es images/volgy2.svg.
 		}
 	}
 
