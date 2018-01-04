277e813c8de63b8291a962a2f3400a756b07e311
smeagolthellama <magardner2010@gmail.com>
2018-01-04 14:18:34 +0200

    add signal handling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string.h>
 #include <cstdlib>
+#include <csignal>
 #include "include/water_map.h"
 
 #ifdef __linux
@@ -14,15 +15,37 @@ extern "C"{
 #endif
 
 using std::cout;
+using std::cerr;
 using std::endl;
+
+float t;
+void react_to_signals(int signum){
+	switch(signum){
+		case 10/*usr1*/:
+			cerr<<"To kill this program, use sigint. To output the current timestep to stderr (eg: if you are piping to a file), use sigusr2. To output this help, use sigusr1."<<endl;
+			break;
+		case 12:
+			cerr<<t<<endl;
+			break;
+		case 2:
+			exit(0);
+			break;
+		default:
+			cerr<<"This... should not be happening. Please tell my developer.";
+			exit(12);
+	}
+}
 
 int main()
 {
+	signal(SIGINT,react_to_signals);
+	signal(SIGUSR1,react_to_signals);
+	signal(SIGUSR2,react_to_signals);
+	signal(SIGSEGV,react_to_signals);
 	water_map w(250,250,0.6);
 	w.graph();
 	getch();
 	int i;
-	float t;
 	for(i=0;i<100000;i++){
 		t=w.step();
 		if(i%100==0){
