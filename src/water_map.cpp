e6e2c0d809679d138736dd08aae2a2cecd5202bb
smeagolthellama <magardner2010@gmail.com>
2018-01-08 17:33:42 +0200

    more

--- src/water_map.cpp
+++ src/water_map.cpp
@@ -235,7 +235,7 @@ double water_map::step()
 						flag|=4;
 					}
 					if(std::isnan(m_map[i][j].delta_water_height)){
-						printf("nan found: d_a: %f; d_b: %f; d_ab: %f; d_w:%f\n",delta_a,delta_b,delta_ab,d_w);
+						printf("nan found: d_a: %f; d_b: %f; d_ab: %f; d_w:%f; cellsize:%f;timestep:%f \n",delta_a,delta_b,delta_ab,d_w,cell_size_si,timestep);
 						throw PROGRAMMING_PANIC;
 					}
 					//TO DO (mark#7#30/12/17): friction
