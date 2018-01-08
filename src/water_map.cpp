6c04a19f06fef0dab4a3f26e8072ff0b0f0b62f2
smeagolthellama <magardner2010@gmail.com>
2018-01-08 17:24:10 +0200

    EVEN more of the previous

--- src/water_map.cpp
+++ src/water_map.cpp
@@ -234,7 +234,9 @@ double water_map::step()
 						}
 						flag|=4;
 					}
-					
+					if(isnan(m_map[i][j].delta_water_height)){
+						printf("nan found: d_a: %f; d_b: %f; d_ab: %f; d_w:%f\n",delta_a,delta_b,delta_ab,d_w);
+					}
 					//TO DO (mark#7#30/12/17): friction
 					float h=m_map[i][j].water_height-m_map[i][j].land_height;
 					float u=2*sqrt(vv);
